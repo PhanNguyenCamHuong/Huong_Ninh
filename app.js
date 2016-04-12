b65c41ccd01f05131678af4f817bfbb0b413f12f
PhanNguyenCamHuong <1312273@student.hcmus.edu.vn>
2016-04-12 08:38:53 +0700

    Added files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var express = require('express');
+
+mongoose.connect('mongodb://localhost',function (err){
+if(err) throw err;
+console.log('connected!');
+
+var app = express();
+app.get('/',function(req, res){
+ res.send(200, 'hello mongoose blog');
+ })
+ app.listen(3000, function() {
+  console.log('now listening on http://localhost:3000');
+  })
+  })
+
